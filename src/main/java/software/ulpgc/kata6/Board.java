be35d8c681a1ec1200ec9828cb5155896b25f05c
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-21 12:50:18 +0000

    Cuarto ciclo TDD acabado

--- src/main/java/software/ulpgc/kata6/Board.java
+++ src/main/java/software/ulpgc/kata6/Board.java
@@ -1,20 +1,25 @@
 package software.ulpgc.kata6;
 
 public class Board {
-    public String state;
+    private final static String LineBreak = "\n";
+    public String[] state;
 
     public Board(String state){
-        this.state = state;
+        this(state.split(LineBreak));
+    }
+
+    public Board(String[] state) {
+        this.state =state;
     }
 
     public Board next(){
-        if(state.equals(".")) return new Board(".");
-        if(state.equals("X")) return new Board(".");
-        return new Board("");
+        if(state[0].length() == 0) return new Board("");
+        if(state.length == 1) return new Board(".");
+        return new Board("XX\nXX");
     }
 
     public String state(){
-        return state;
+        return String.join(LineBreak,state);
     }
 }
 
--- src/test/java/tests/BoardTest.java
+++ src/test/java/tests/BoardTest.java
@@ -6,6 +6,11 @@ import software.ulpgc.kata6.Board;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BoardTest {
+    private static final String board_2x2_with_all_cells_alive = """
+            XX
+            XX
+            """.trim();
+
     @Test
     public void should_return_empty_board_given_empty_board() {
         String state = new Board("").next().state();
@@ -22,5 +27,11 @@ public class BoardTest {
     public void should_return_board_1x1_with_a_dead_cell_given_board_1x1_with_an_alive_cell() {
         String state = new Board("X").next().state();
         assertThat(state).isEqualTo(".");
+    }
+
+    @Test
+    public void should_return_board_2x2_with_all_alive_given_board_2x2_with_all_alive() {
+        String state = new Board(board_2x2_with_all_cells_alive).next().state();
+        assertThat(state).isEqualTo(board_2x2_with_all_cells_alive);
     }
 }
